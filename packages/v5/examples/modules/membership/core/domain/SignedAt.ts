dac5aa712407bd43036f822779fd20e49ca80f81
Jens van Hellemondt <jens@invictus.codes>
2026-05-05 21:06:51 +0200

    feat: update SignedAt to allow offset

--- packages/v5/examples/modules/membership/core/domain/SignedAt.ts
+++ packages/v5/examples/modules/membership/core/domain/SignedAt.ts
@@ -1,4 +1,4 @@
 import { z } from 'zod';
 
-export const signedAt = z.iso.datetime().brand<"SignedAt">();
+export const signedAt = z.iso.datetime({ offset: true }).brand<"SignedAt">();
 export type SignedAt = z.infer<typeof signedAt>;
