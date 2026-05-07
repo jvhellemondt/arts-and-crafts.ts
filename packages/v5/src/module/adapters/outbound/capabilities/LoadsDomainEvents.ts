3bea48785f832c679260b8f9ae87b103f29d90fb
Jens van Hellemondt <jens@invictus.codes>
2026-05-08 00:33:36 +0200

    refactor(LoadsDomainEvents): update to return specific events

--- packages/v5/src/module/adapters/outbound/capabilities/LoadsDomainEvents.ts
+++ packages/v5/src/module/adapters/outbound/capabilities/LoadsDomainEvents.ts
@@ -1,5 +1,5 @@
 import type { DomainEvent } from "@core/shapes/DomainEvent.ts";
 
-export interface LoadsDomainEvents<TResult = Promise<DomainEvent>[]> {
+export interface LoadsDomainEvents<TEvent extends DomainEvent, TResult = Promise<TEvent[]>> {
   load(streamName: string, aggregateId: string): TResult;
 }
