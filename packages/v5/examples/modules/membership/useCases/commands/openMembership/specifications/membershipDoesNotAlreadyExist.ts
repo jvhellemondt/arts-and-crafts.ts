a7f98cd0cada20491491cde0469bc306acb3a74d
Jens van Hellemondt <jens@invictus.codes>
2026-05-10 21:55:20 +0200

    chore: remove deleted file from git cache as well

--- packages/v5/examples/modules/membership/useCases/commands/openMembership/specifications/membershipDoesNotAlreadyExist.ts
+++
@@ -1,8 +0,0 @@
-import type { EvaluateCandidate } from "@core/capabilities/EvaluateCandidate.ts";
-import type { MembershipState } from "@examples/modules/membership/core/state.ts";
-
-export class MembershipDoesNotAlreadyExist implements EvaluateCandidate<MembershipState> {
-  isSatisfiedBy(candidate: MembershipState): boolean {
-    return candidate.status === "initial";
-  }
-}
