7ab80f87b30c74ee921a98d24753617e40b52957
Jens van Hellemondt <jens@invictus.codes>
2026-05-21 21:08:28 +0200

    refactor(OpenMembershipRejected): implement new Notification interface

--- packages/v5/examples/modules/membership/useCases/commands/openMembership/rejections/MembershipAlreadyExists.ts
+++ packages/v5/examples/modules/membership/useCases/commands/openMembership/rejections/MembershipAlreadyExists.ts
@@ -1,5 +1,14 @@
 import type { Rejection } from "@core/shapes/Rejection.ts";
+import type { Notification } from "@adapters/outbound/shapes/Notification.ts";
+import type { OpenMembershipCommandPayload } from "../command.ts";
 
 export interface MembershipAlreadyExists extends Rejection<"MEMBERSHIP_ALREADY_EXISTS"> {
   reason: "Membership already exists";
 }
+
+export type OpenMembershipRejected = Notification<
+  "OpenMembershipRejected",
+  OpenMembershipCommandPayload,
+  "MEMBERSHIP_ALREADY_EXISTS",
+  MembershipAlreadyExists
+>;
