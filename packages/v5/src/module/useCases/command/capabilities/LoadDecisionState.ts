8fcdf98c226ce2a01b12d381956394e2df2f23d7
Jens van Hellemondt <jens@invictus.codes>
2026-06-15 20:38:23 +0200

    refactor: LoadDecisionState should enable multi ids

--- packages/v5/src/module/useCases/command/capabilities/LoadDecisionState.ts
+++ packages/v5/src/module/useCases/command/capabilities/LoadDecisionState.ts
@@ -1,5 +1,3 @@
-import type { StreamKey } from "@adapters/outbound/shapes/StreamKey.ts";
-
 export interface LoadDecisionState<TState, TReturn = Promise<TState>> {
-  load(criteria: readonly StreamKey[]): TReturn;
+  load(id: string, ...otherIds: string[]): TReturn;
 }
